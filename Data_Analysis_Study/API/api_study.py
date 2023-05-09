c4770f15015a35dd0ae15ea8ed3551a1b04da07f
minkyeom <minkyeom@naver.com>
2023-05-09 18:36:40 +0900

    Data_Analysis

--- Data_Analysis_Study/API/api_study.py
+++ Data_Analysis_Study/API/api_study.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 d={"name": "혼자 공부하는 파이썬"}
 print(d["name"])
@@ -27,4 +28,51 @@ d4_str="""
 
 d4=json.loads(d4_str)
 print("세겹 따옴표",d4[1]["name"])
+print(pd.DataFrame(d4))
 
+# xml 문자열
+def xml_sol():
+    import xml.etree.ElementTree as et
+    x_str="""
+    <book>
+    <name>혼자 공부하는 데이터 분석</name>
+    <author>박해선</author>
+    <year>2023</year>
+    </book>
+    """
+    book =et.fromstring(x_str)
+    return book
+
+print(type(xml_sol()))
+a=xml_sol()
+book_childs=list(a)
+print(book_childs)
+name, author,year=book_childs
+print(name.text)
+author_2=a.findtext("author")
+print(author_2)
+# 여러개의 엘리먼트
+def xml_sol2():
+    import xml.etree.ElementTree as et
+    x_str2 = """
+    <books>
+        <book>
+            <name>혼자 공부하는 데이터 분석</name>
+            <author>박해선</author>
+            <year>2023</year>
+        </book>
+        <book>
+            <name>혼자 공부하는 데이터 분석2</name>
+            <author>박해선2</author>
+            <year>2023(2)</year>
+        </book>
+     </books>
+     """
+    book2=et.fromstring(x_str2)
+    return book2
+
+b= xml_sol2()
+
+for book in b.findall("book"):
+    name=book.findtext("name")
+    print(name)
